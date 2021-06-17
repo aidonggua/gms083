06d19f05dd5ee2a231b621c3aa9bbe6bff6c1bfa
叶浩 <77729052@qq.com>
2021-06-18 01:00:41 +0800

    .

--- src/main/java/client/command/commands/gm2/SpCommand.java
+++ src/main/java/client/command/commands/gm2/SpCommand.java
@@ -27,6 +27,7 @@ import client.command.Command;
 import client.MapleClient;
 import client.MapleCharacter;
 import config.YamlConfig;
+import constants.game.GameConstants;
 
 public class SpCommand extends Command {
     {
@@ -46,7 +47,8 @@ public class SpCommand extends Command {
             if (newSp < 0) newSp = 0;
             else if (newSp > YamlConfig.config.server.MAX_AP) newSp = YamlConfig.config.server.MAX_AP;
 
-            player.updateRemainingSp(newSp);
+            // player.updateRemainingSp(newSp);
+            player.gainSp(newSp, GameConstants.getSkillBook(player.getJob().getId()), true);
         } else {
             MapleCharacter victim = c.getWorldServer().getPlayerStorage().getCharacterByName(params[0]);
             if (victim != null) {
