457752aeccd22ec7f939b3d3a03eb3b8f92e02e6
root <root@localhost.localdomain>
2021-06-17 21:37:38 +0800

    .

--- scripts/npc/paimai.js
+++ scripts/npc/paimai.js
@@ -3,23 +3,30 @@
 importPackage(Packages.client.command);
 
 var status;
-var menu = ["传送"]
-var mapNames = ["彩虹村", "明珠港", "魔法密林", "射手村", "废弃都市", "勇士部落"]
-var mapIds = [1000000, 100000000, 101000000, 130000000, 103000000, 102000000]
+var menu = ["传送", "专职"]
+var maps = [
+    {name: "彩虹村", code: 1000000},
+    {name: "明珠港", code: 104000000},
+    {name: "魔法密林", code: 101000000},
+    {name: "射手村", code: 100000000},
+    {name: "废弃都市", code: 103000000},
+    {name: "勇士部落", code: 102000000}
+]
 
 function handleMenu() {
     var sendStr = "快捷服务：\r\n\r\n#b";
     for (var i = 0; i < menu.length; i++) {
         sendStr += "#L" + i + "#" + menu[i] + "#l\r\n";
     }
-    return sendStr
+    return sendStr;
 }
 
 function handleMap() {
     var sendStr = "地图：\r\n\r\n#b";
-    for (var i = 0; i < mapNames.length; i++) {
-        sendStr += "#L" + (1000 + i) + "#" + mapNames[i] + "#l\r\n";
+    for (var i = 0; i < maps.length; i++) {
+        sendStr += "#L" + (1000 + i) + "#" + maps[i].name + "#l\r\n";
     }
+    return sendStr;
 }
 
 function start() {
@@ -40,18 +47,17 @@ function action(mode, type, selection) {
             cm.sendSimple(handleMenu());
         } else if (status == 1) {
             if (selection == 0) {
-                cm.sendPrev(handleMap());
+                cm.sendSimple(handleMap());
             } else {
-                cm.sendPrev("功能待完善");
+                cm.sendSimple("功能待完善");
             }
         } else if (status == 2) {
-            cm.sendPrev("debug: " + selection);
             if (selection >= 1000 && selection < 2000) {
-                var mapId = mapIds[selection - 1000]
-                cm.getPlayer().changeMap(mapId)
+                var mapId = maps[selection - 1000].code;
+                cm.getPlayer().changeMap(mapId);
                 cm.dispose();
             } else {
-                cm.sendPrev("功能待完善");
+                cm.sendSimple("功能待完善");
             }
         } else {
             cm.dispose();
