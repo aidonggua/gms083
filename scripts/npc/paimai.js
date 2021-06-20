fcf6b81817e50d72d880fad22e43bf2cefe8f7db
叶浩 <77729052@qq.com>
2021-06-20 19:44:07 +0800

    .

--- scripts/npc/paimai.js
+++ scripts/npc/paimai.js
@@ -5,12 +5,17 @@ importPackage(Packages.client.command);
 var status;
 var menu = ["传送", "专职"]
 var maps = [
+    {name: "自由市场", code: 910000000},
     {name: "彩虹村", code: 1000000},
     {name: "明珠港", code: 104000000},
     {name: "魔法密林", code: 101000000},
     {name: "射手村", code: 100000000},
     {name: "废弃都市", code: 103000000},
-    {name: "勇士部落", code: 102000000}
+    {name: "勇士部落", code: 102000000},
+    {name: "天空之城", code: 104000000},
+    {name: "阿里安特", code: 260000000},
+    {name: "童话村", code: 222000000},
+    {name: "武陵", code: 250000000}
 ]
 
 function handleMenu() {
