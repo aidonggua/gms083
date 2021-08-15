dc4a47b6a1604f76223533e8a2280aa0f607756c
叶浩 <77729052@qq.com>
2021-08-15 20:46:20 +0800

    .

--- linux-launch.sh
+++ linux-launch.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 # cores in classpath, thanks to lkxyyjx
-export CLASSPATH=".:dist/*:cores/*"
-java -Xmx2048m -Dwzpath=wz/ net.server.Server
+#export CLASSPATH=".:dist/*:cores/*"
+#java -Xmx2048m -Dwzpath=wz/ net.server.Server
+
+
+./target/HeavenMS/bin/start.sh
